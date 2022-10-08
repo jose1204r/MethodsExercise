a7e226b67b12fb9f8187d35931c5b66158c78e3a
jose <jrodriguez@itsolutionspcllc.com>
2022-10-07 22:22:43 -0400

    Began user questions

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace MethodsExercise
+﻿using System.Drawing;
+
+namespace MethodsExercise
 {
     internal class Program
     {
         static void Main(string[] args)
+
+
         {
-            Console.WriteLine("Hello, World!");
+            
+
+          Console.WriteLine("Hello! - What is your name ? ");
+            var userName = Console.ReadLine();
+
+            Console.WriteLine($"hi {userName}- What is your favorite color ?");
+            var userFavoritecolor = Console.ReadLine();
+
+            Console.WriteLine($"Nice color {userName} and - What is your favorite animal ? ");
+            var userFavoriteanimal = Console.ReadLine();
+
+            Console.WriteLine($" great! {userName} I love animals - What is your favorite band ?");
+            var userFavoriteband = Console.ReadLine();
+
+
+
+
         }
     }
 }
