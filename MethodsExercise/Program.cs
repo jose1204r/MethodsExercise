8aa9a26f64d924f382ea1f42bc1947af527ebb5b
jose <jrodriguez@itsolutionspcllc.com>
2022-10-08 16:30:50 -0400

    Method Exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,11 +4,59 @@ namespace MethodsExercise
 {
     internal class Program
     {
+        // five methods
+
+
+        public static int sumNumber (int a, int b)
+        {
+            var anwserNubers = a + b;
+            return anwserNubers;
+        }
+
+        public static int Multiply(int num1, int num2, int num3)
+        {
+            return num1 * num2 * num3;
+        }
+
+        public static int subtractMethod(int a1, int b2) {
+
+            return a1 - b2;
+
+
+        }
+        public static int divideMethod(int c, int d) {
+
+            return c / d;
+
+
+        }
+
+
+        public static int modulosMethod(int e, int f) {
+
+            return e % f;
+
+
+        }
+
+
+
+
         static void Main(string[] args)
 
 
         {
-            
+
+         // five mothods called
+
+            var sumresult = sumNumber(4, 10);
+            var resultMultipy = Multiply(10, 20, 100);
+            var subtResult = subtractMethod(57, 68);
+            var divResult = divideMethod(20,10);
+            var modResult = modulosMethod(30, 50);
+
+          
+            // --------Excercie 1---------------------------------
 
           Console.WriteLine("Hello! - What is your name ? ");
             var userName = Console.ReadLine();
@@ -33,6 +81,8 @@ namespace MethodsExercise
 
             Console.WriteLine($"Favorite Band : {userFavoriteband}");
             
+
+           
 
 
 
